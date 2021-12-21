adf65972a8ac0eec1a87cb5219c63e81c09f2160
maurethi <maurethi@students.zhaw.ch>
2021-12-21 20:59:45 +0000

    dfddaass

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -217,7 +217,23 @@ export class GuiModel {
 
                     ]
                 },
-                { "id": "groupspage", "elementList": [ { "type": "backbutton", }, { "type": "newButton", "name": "NewGroup", "icon": "fa-weixin", "color": "green", "form": { "form": "GroupForm" } } ] }
+                { "id": "groupspage", "elementList": [ 
+                    { "type": "backbutton", }, 
+                    { "type": "newButton", "name": "NewGroup", "icon": "fa-weixin", "color": "green", "form": 
+                    { "form": "GroupForm" } 
+                    },
+                    {
+                        "type": "list",
+                        "icon": "fa-weixin",
+                        "color": "wisteria",
+                        "search": true,
+                        "url": "/group",
+                        "form": {
+                        "form": "GroupForm"
+                        }
+                        },
+                    ] 
+            }
             ]
         }
     };
